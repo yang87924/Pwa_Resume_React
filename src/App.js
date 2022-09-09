eea01ba5abff94c6ec0ce2c91ccc2b1c3721e918
yang <luca@trunk-studio.com>
2022-09-10 00:15:29 +0800

    first push heroku

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="qwe">
+      <h1>基本資料basic information</h1>
+      <img className='img'alt=""src="https://user-images.githubusercontent.com/102047848/187385756-510d8cdb-3587-4ed4-af2c-b4ca10454a80.JPG"width={100}height={100}/><br/>
+      <div className='asad'>
+        姓名:洪偉哲<br/>
+        信箱:4a690103@office.stust.edu.tw<br/>
+        Github: https://github.com/yang87924<br/>
+      </div>
+    
+      <h1>簡歷 Summary</h1>
+      我叫洪偉哲，畢業於南臺科技大學資管系，我對程式有熱忱且願意學習新技術，其中比較擅長APP部分，我很享受撰寫程式碼的感覺，對於Android開發有經驗
+      <h1>工作經驗 work experience</h1>
+      <ul >
+        <li>(C#)程式設計課程 TA。(2022/03 ~ 2022/06)</li>
+        <li>南臺科大註冊組工讀生。(2022/03 ~ 2022/06)</li>
+        <li>109 智慧創新跨域人才培育聯盟計畫-專任助教。(2020.08 ~ 2020.12)</li>
+        <li>會計學 TA。(2020/03 ~ 2020/06)</li>
+      </ul>
+      <h1>證照 Certificates</h1>
+      <ul>
+        <li>電腦硬體裝修(丙級)</li>
+        <li>勞動部電腦硬體裝修丙級</li>
+        <li>TQC 電子商務概論專業級</li>
+        <li>TQC+網頁編輯製作(Dreamweaver CC)</li>
+        <li>EEC 企業電子化助理規劃師</li>
+        <li>EEC 企業電子化規劃師-網路行銷</li>
+        <li>TQC+ 影像編輯製作(Photoshop CC)</li>
+        <li>ERP 規劃師</li>
+        <li>ICDL Data Analytics</li>
+        <li>ICDL Computing</li>
+      </ul>
+      <h1>專業技能 Skills</h1>
+      <img className='img'alt=""src="https://user-images.githubusercontent.com/102047848/187385972-0c144367-670d-4c73-bbd9-f489e0a459c6.png"width={100}height={100}/>
+      <img className='img'alt=""src="https://user-images.githubusercontent.com/102047848/187386009-98acc5f1-fb19-4c91-9493-a223236edf73.png"width={100}height={100}/>
+      <img className='img'alt=""src="https://user-images.githubusercontent.com/102047848/187386067-006c0255-f19a-411f-a7b6-bf432be27e68.png"width={100}height={100}/>
+      <img className='img'alt=""src="https://user-images.githubusercontent.com/102047848/187394449-aead1e27-b613-4cb8-8472-c0bbf993f4d3.png"width={100}height={100}/>
+      <h1>學習經歷 learning experience</h1>
+      <ul>
+        <li>大四
+          <ul>
+            <li>參加 2021 ITSA 極客挑戰賽:資訊系統與網頁設計應用比賽入決賽</li>
+            <li>參加 110 資訊智慧創新跨域專題競賽(甲等)</li>
+            <li>擔任程式設計(C#)TA</li>
+            <li>擔任南臺科技大學註冊組夜間部工讀生</li>
+          </ul>
+        </li>
+        <li>大三
+          <ul>
+            <li>擔任 109 智慧創新跨域人才培育聯盟計畫-專任助教</li>
+            <li>參加 109 年教育部智慧創新跨域人才培育計畫-南一區微學程交流觀摩活動</li>
+            <li>參加 2020 產學科技論壇-跨域先行者活動</li>
+            <li>參加大數據分析程式訓練課程(R)</li>
+            <li>參加 2020 ITSA 全國大專程式設計極客挑戰賽</li>
+          </ul>
+        </li>
+        <li>大二
+          <ul>
+            <li>參加 ITSA 程式訓練課程(python)</li>
+            <li>擔任會計學 TA</li>
+          </ul>
+        </li>
+        <li>大一
+          <ul>
+            <li>參加 ITSA 程式訓練課程(JAVA)</li>
+          </ul>
+        </li>
+      </ul>
+     
+    <h1>學歷 Education</h1>
+    <ul>
+      <li>南臺科技大學 2018/09 - 2022/06
+        <ul>
+          <li>資訊管理系</li>
+        </ul>
+      </li>
+
+      <li>國立永靖高工 2014/09 - 2017/06
+        <ul>
+          <li>資訊科</li>
+        </ul>
+      </li>
+    </ul>
+   
+    <h1>語言能力 language skills</h1>
+    <ul>
+      <li>中文(母語)</li>
+      <li>英文(初學)</li>
+    </ul>
     </div>
   );
 }
